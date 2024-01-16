3fec5e28c007fd8ca8a8e4308b0839fe31966b2a
Sachin Gupta <sachinjava20@gmail.com>
2024-01-17 02:39:43 +0530

    Update loginController.js

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -73,5 +73,10 @@ export const login = async (req, res) => {
       });
     }
   };
-  
+  export const logout = async(req,res) => {
+    res.status(200).json({
+        success:true,
+        message: 'Logout successful' });
+    res.end();
+}
   
