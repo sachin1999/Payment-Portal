4bec0e001af637b9237f09fe9531c8989de820b6
Sachin Gupta <sachinjava20@gmail.com>
2024-01-17 02:38:03 +0530

    Update loginRoute.js

--- server/routes/loginRoute.js
+++ server/routes/loginRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { details, login } from "../controllers/loginController.js";
+import { details, login, logout } from "../controllers/loginController.js";
 
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/details", details);
+router.post('/logout', logout)
 
 export default router;
